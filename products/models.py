10d89830606725c7bf10f7040c9498fd3c6325a5
OatsRx <otis.redman1992@gmail.com>
2020-10-17 12:04:29 +0000

    rewrote product fixtures due to issue with json data

--- products/models.py
+++ products/models.py
@@ -1,27 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class Category(models.Model):
-    name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
-    def friendly_name(self):
-        return self.friendly_name
-
-
-class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
-    name = models.CharField(max_length=254)
-    rarity = models.CharField(max_length=254, null=True, blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    product_type = models.CharField(max_length=254)
-    image = models.ImageField(null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
