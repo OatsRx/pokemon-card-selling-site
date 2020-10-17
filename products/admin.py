f354005e45c9622d45a929208fa8a40be4544dfa
OatsRx <otis.redman1992@gmail.com>
2020-10-17 14:34:46 +0000

    made admin product and category viewing semantic

--- products/admin.py
+++ products/admin.py
@@ -2,7 +2,26 @@ from django.contrib import admin
 from .models import Category, Product
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
 
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'price',
+        'category',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
