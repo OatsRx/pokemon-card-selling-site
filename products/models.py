fda1137a7de6046201f2703516f8831cbe7cfe59
OatsRx <otis.redman1992@gmail.com>
2020-10-20 12:34:26 +0000

    pulled products from db to template

--- products/models.py
+++ products/models.py
@@ -22,7 +22,7 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
-    rarity = models.CharField(max_length=254)
+    rarity = models.CharField(max_length=254, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
