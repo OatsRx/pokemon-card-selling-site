48bda73227a6c5e58b3d1311590e860b9e542ff7
OatsRx <otis.redman1992@gmail.com>
2020-10-21 14:40:20 +0000

    added shopping bag content and links

--- shoppingbag/views.py
+++ shoppingbag/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def index(request):
-    """A view to render the shopping bag"""
+def view_shopping_bag(request):
+    """A view to render the shopping bag list"""
 
     return render(request, 'shoppingbag/shopping_bag.html')
 
