d9220a147b820c613563d89b37e5a3194dba0d6b
OatsRx <otis.redman1992@gmail.com>
2020-10-23 14:01:35 +0000

    started adding products to bag with viewws and context

--- shoppingbag/contexts.py
+++ shoppingbag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def shopping_bag_contents(request):
@@ -10,6 +12,18 @@ def shopping_bag_contents(request):
     total = 0
     product_count = 0
     delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+    shopping_bag = request.session.get('shopping_bag', {})
+
+    for item_id, quantity in shopping_bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        shopping_bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
     grand_total = delivery + total
 
     context = {
