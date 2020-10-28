17ba9ed02be9784e9f583244bcaab9abf7f69603
OatsRx <otis.redman1992@gmail.com>
2020-10-28 22:58:47 +0000

    added stripe webhooks

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('payment_success/<order_number>', views.payment_success, name='payment_success'),
-
+    path('wh/', webhook, name='webhook'),
 ]
