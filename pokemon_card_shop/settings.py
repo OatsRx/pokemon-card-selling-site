0205caadd9de61cdab71962a1952699afa62a9d3
OatsRx <otis.redman1992@gmail.com>
2020-10-30 22:56:16 +0000

    3rd deployment stage

--- pokemon_card_shop/settings.py
+++ pokemon_card_shop/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+-m2405hb(n$s!$83szp3ht)*mix+s2gc)$@9+97vioajrv#k$'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'flamin-pidgey-card-store.herokuapp.com',
