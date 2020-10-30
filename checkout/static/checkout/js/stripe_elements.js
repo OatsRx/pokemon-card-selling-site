e72133709a99159d7735f704b219add370be55f2
OatsRx <otis.redman1992@gmail.com>
2020-10-30 14:58:38 +0000

    added user profile, login and edited allauth templates

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,11 +1,7 @@
-/* Payment logic from stripe documentation */
-
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
-
-/* Styles from stripe documentation */
 var style = {
     base: {
         color: '#000',
@@ -21,20 +17,36 @@ var style = {
         iconColor: '#dc3545'
     }
 };
-
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
-/* Form submit logic from stripe documentation and modified */
+// Handle validation errors 
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+// Handle form submit
 var form = document.getElementById('payment-form');
 
-
-form.addEventListener('submit', function (ev) {
+form.addEventListener('submit', function(ev) {
     ev.preventDefault();
-    card.update({ 'disabled': true });
+    card.update({ 'disabled': true});
     $('#confirm-payment').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
 
-    var saveInfo = Boolean($('#id-save-details').attr('checked'));
+    var saveInfo = Boolean($('#id-save-info').attr('checked'));
     var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
     var postData = {
         'csrfmiddlewaretoken': csrfToken,
@@ -47,29 +59,43 @@ form.addEventListener('submit', function (ev) {
         stripe.confirmCardPayment(clientSecret, {
             payment_method: {
                 card: card,
-                billing_detaill: {
-                    name: (form.full_name.value),
-                    phone: (form.phone_number.value),
-                    email: (form.email.value),
-                    address: {
-                        line1: (form.street_address1.value),
-                        line2: (form.street_address2.value),
-                        city: (form.town_or_city.value),
-                        country: (form.country.value),
-                        county: (form.county.value),
+                billing_details: {
+                    name: $.trim(form.full_name.value),
+                    phone: $.trim(form.phone_number.value),
+                    email: $.trim(form.email.value),
+                    address:{
+                        line1: $.trim(form.street_address1.value),
+                        line2: $.trim(form.street_address2.value),
+                        city: $.trim(form.town_or_city.value),
+                        country: $.trim(form.country.value),
+                        state: $.trim(form.county.value),
                     }
                 }
-            }
-        }).then(function (result) {
+            },
+            shipping: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                address: {
+                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
+                    city: $.trim(form.town_or_city.value),
+                    country: $.trim(form.country.value),
+                    postal_code: $.trim(form.postcode.value),
+                    state: $.trim(form.county.value),
+                }
+            },
+        }).then(function(result) {
             if (result.error) {
                 var errorDiv = document.getElementById('card-errors');
                 var html = `
-                <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
-                </span>
-                <span>${result.error.message}</span>`;
+                    <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                    </span>
+                    <span>${result.error.message}</span>`;
                 $(errorDiv).html(html);
-                card.update({ 'disabled': false });
+                $('#payment-form').fadeToggle(100);
+                $('#loading-overlay').fadeToggle(100);
+                card.update({ 'disabled': false});
                 $('#confirm-payment').attr('disabled', false);
             } else {
                 if (result.paymentIntent.status === 'succeeded') {
@@ -78,7 +104,6 @@ form.addEventListener('submit', function (ev) {
             }
         });
     }).fail(function () {
-        // fail/400 request
         location.reload();
     })
 });
--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
 
 def user_profile(request):
     """ A view to display the users profile """
 
+    user_profile = get_object_or_404(UserProfile)
     context = {
-
+        'user_profile': user_profile
     }
     return render(request, 'profiles/user_profile.html', context)
 
