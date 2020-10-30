c83caef6f03416d5f2f32ff16c95d44f9fd6ef01
OatsRx <otis.redman1992@gmail.com>
2020-10-30 22:44:36 +0000

    2nd Deployment stage

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pokemon_card_shop.wsgi:application
--- pokemon_card_shop/settings.py
+++ pokemon_card_shop/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = '+-m2405hb(n$s!$83szp3ht)*mix+s2gc)$@9+97vioajrv#k$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'flamin-pidgey-card-store.herokuapp.com',
+    'localhost',
+]
 
 
 # Application definition
@@ -124,11 +127,16 @@ WSGI_APPLICATION = 'pokemon_card_shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
- DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
 }
 
 
