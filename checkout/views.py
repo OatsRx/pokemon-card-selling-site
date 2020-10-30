cda8766f3176ad925954552ac936e5a623d28e56
OatsRx <otis.redman1992@gmail.com>
2020-10-30 17:45:48 +0000

    added functionality to saving delivery details

--- checkout/views.py
+++ checkout/views.py
@@ -71,6 +71,24 @@ def checkout(request):
         currency=settings.STRIPE_CURRENCY,
     )
 
+    if request.user.is_authenticated:
+        try:
+            profile = UserProfile.objects.get(user=request.user)
+            order_form = OrderForm(initial={
+                'full_name': profile.user.get_full_name(),
+                'email': profile.user.email,
+                'phone_number': profile.phone_number,
+                'country': profile.country,
+                'postcode': profile.postcode,
+                'town_or_city': profile.town_or_city,
+                'street_address1': profile.street_address1,
+                'street_address2': profile.street_address2,
+            })
+        except UserProfile.DoesNotExist:
+            order_form = OrderForm()
+    else:
+        order_form = OrderForm()
+
     order_form = OrderForm()
     context = {
         'order_form': order_form,
@@ -94,16 +112,14 @@ def payment_success(request, order_number):
         order.user_profile = profile
         order.save()
 
-        # Save the user's info
         if save_info:
             profile_data = {
-                'default_phone_number': order.phone_number,
-                'default_country': order.country,
-                'default_postcode': order.postcode,
-                'default_town_or_city': order.town_or_city,
-                'default_street_address1': order.street_address1,
-                'default_street_address2': order.street_address2,
-                'default_county': order.county,
+                'phone_number': order.phone_number,
+                'country': order.country,
+                'postcode': order.postcode,
+                'town_or_city': order.town_or_city,
+                'street_address1': order.street_address1,
+                'street_address2': order.street_address2,
             }
             user_profile_form = UserProfileForm(profile_data, instance=profile)
             if user_profile_form.is_valid():
